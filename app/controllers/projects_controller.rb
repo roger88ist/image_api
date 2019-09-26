3eee7de6195232ebc30d5ce174db888a2fe177de
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-09-25 22:29:55 -0400

    created the ProjectsController with index and create actions with an index view. Also added routes to that controller via resources for now. They will be trimmed later for the api service

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,18 @@
+class ProjectsController < ApplicationController
+  
+  def index
+    @projects = Project.all
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :creator_email)
+  end
+end
