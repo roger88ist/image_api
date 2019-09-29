59be9c2c41affc4c442e644937c7843703998d1c
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-09-29 15:38:12 -0400

    added Project validations to check the presence of name, description, creator_image, and image

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ApplicationRecord
+  
   has_one_attached :image
+  
+  validates :name, :description, :creator_email, presence: true
+  validate :image_presence
+
+  private 
+
+  def image_presence
+    unless self.image.attached?
+      errors.add(:image, "must be present.")
+    end
+  end
 end
