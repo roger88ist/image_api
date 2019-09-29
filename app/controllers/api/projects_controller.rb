5eb033b1ea10bb2933bf23391fdd672d4c5c25fe
Rogerio Istiraneopulos <roger88ist@gmail.com>
2019-09-29 14:22:42 -0400

    created Api::ProjectsController#create and before_action to allow client side code to access the response

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,6 +1,28 @@
 class Api::ProjectsController < ActionController::API
 
+  before_action :allow_response_access, only: [:create]
+
   def index
     render json: Project.all
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      render json: @project, status: :created
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :creator_email, :image)
+  end
+
+  def allow_response_access
+    response.set_header('Access-Control-Allow-Origin', '*')
   end
 end
